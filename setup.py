ea53a00ee50b1047caeb286d0b877ff764fad658
Niarfe <efrain.olivares@gmail.com>
2019-01-06 03:56:03 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='addext',
+    version='0.1',
+    description='Address Extractor',
+    url='https://github.com/Niarfe/addext',
+    author='Efrain Olivares',
+    author_email='efrain.olivares@gmail.com',
+    license='MIT',
+    packages=['addext'],
+    zip_safe=False
+    )
