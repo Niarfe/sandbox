fe38bda0d94b560a23ab8264d25d46c93a0cae4e
Niarfe <efrain.olivares@gmail.com>
2019-01-06 04:22:25 -0800

    release tasks for invoke

--- tasks.py
+++ tasks.py
@@ -3,3 +3,8 @@ from invoke import task
 @task
 def tests(c):
     c.run("py.test -vvx tests", pty=True)
+
+@task
+def release(c):
+    c.run("python3 setup.py sdist bdist_wheel")
+    c.run("python3 -m twine upload dist/*")
